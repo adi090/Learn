d69f762ca9f751098f4b03f1131d99b87269d422
Aditya Prasannan <adityaprasanan2003@gmail.com>
2025-01-13 16:55:03 +0530

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,128 @@
-import { School } from "lucide-react";
+import { Menu, School } from "lucide-react";
 import React from "react";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuSubContent,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuGroup,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuPortal,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import DarkMode from "@/DarkMode";
+import { Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger, } from "./ui/sheet";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 function Navbar() {
+  const user = true;
   return (
     <div className="h-16 dark:bg-[#0A0A0A] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
-        {/* Desktop */}
-        <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
-        <School size={"30"}/>
-        <h1 className="hidden md:block font-extrabold text-2xl">E-learning</h1>
+      {/* Desktop */}
+      <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
+        <div className="flex items-center gap-2">
+          <School size={"30"} />
+          <h1 className="hidden md:block font-extrabold text-2xl">
+            E-learning
+          </h1>
         </div>
+        {/* User icons and dark mode icon  */}
+       <div className="flex items-center gap-8">
+       {user ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>My Learning</DropdownMenuItem>
+                <DropdownMenuItem>Edit Profile </DropdownMenuItem>
+                <DropdownMenuItem>Log out</DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem>Dashboard</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <div className="flex items-center gap-2">
+            <Button variant="outline">Login</Button>
+            <Button>Signup</Button>
+          </div>
+        )}
+        <DarkMode/>
+       </div>
+      </div>
+      {/* Mobile  */}
+   <div className="flex md:hidden items-center justify-between px-4 h-full">
+   <h1 className="font-extrabold text-2xl">E-learning</h1>
+   <MobileNavbar/>
+
+   </div>
     </div>
   );
 }
 
 export default Navbar;
+
+
+const MobileNavbar =()=>{
+  return (
+    <Sheet>
+    <SheetTrigger asChild>
+      <Button size='icon' className="rounded-full bg-gray-200 hover:bg-gray-200" variant="outline">
+        <Menu/>
+      </Button>
+    </SheetTrigger>
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Edit profile</SheetTitle>
+        <SheetDescription>
+          Make changes to your profile here. Click save when you're done.
+        </SheetDescription>
+      </SheetHeader>
+      <div className="grid gap-4 py-4">
+        <div className="grid grid-cols-4 items-center gap-4">
+          <Label htmlFor="name" className="text-right">
+            Name
+          </Label>
+          <Input id="name" value="Pedro Duarte" className="col-span-3" />
+        </div>
+        <div className="grid grid-cols-4 items-center gap-4">
+          <Label htmlFor="username" className="text-right">
+            Username
+          </Label>
+          <Input id="username" value="@peduarte" className="col-span-3" />
+        </div>
+      </div>
+      <SheetFooter>
+        <SheetClose asChild>
+          <Button type="submit">Save changes</Button>
+        </SheetClose>
+      </SheetFooter>
+    </SheetContent>
+  </Sheet>
+  )
+}
